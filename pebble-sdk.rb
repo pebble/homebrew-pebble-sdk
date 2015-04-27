62aacb28efb201a391fa1ae5a733f96abfbd6bf7
Cherie Williams <cherie@getpebble.com>
2015-04-27 15:56:22 -0700

    Updating SDK to 3.0-dp9

--- pebble-sdk.rb
+++ pebble-sdk.rb
@@ -1,10 +1,15 @@
 require 'formula'
+require 'curses'
 
 class PebbleSdk < Formula
+  PEBBLE_SDK_REQUIREMENTS=[{'url' => 'https://pypi.python.org/packages/source/f/freetype-py/freetype-py-1.0.tar.gz', 'hash' => '1fc67817d5fb9f1329a1a431850a46f01f250a1d6380e4bcecdb54266023e99a', 'name' => 'freetype-py'}, {'url' => 'https://pypi.python.org/packages/source/s/sh/sh-1.09.tar.gz', 'hash' => 'f3d174e2ad25c39f28935bae672be51aa083063d3122405ceeb2a3e7a8239d45', 'name' => 'sh'}, {'url' => 'https://pypi.python.org/packages/source/T/Twisted/Twisted-12.0.0.tar.bz2', 'hash' => 'e5561c9714583fd052ea9e0b10331609a719b3878f9d2593e4c57eab0ba0effd', 'name' => 'twisted'}, {'url' => 'https://pypi.python.org/packages/source/a/autobahn/autobahn-0.5.14.zip', 'hash' => 'd56979d4dffae6dc4d9c840629b9a8092ac41ea75f55052d70943edf7e01979d', 'name' => 'autobahn'}, {'url' => 'https://pypi.python.org/packages/source/w/websocket-client/websocket-client-0.22.0.tar.gz', 'hash' => '83a173ea32ef4209a1933b3e0f5bfafe533b909073ff8d657f044b5792b8c7b5', 'name' => 'websocket-client'}, {'url' => 'https://pypi.python.org/packages/source/p/pyserial/pyserial-2.6.tar.gz', 'hash' => '049dbcda0cd475d3be903e721d60889ee2cc4ec3b62892a81ecef144196413ed', 'name' => 'pyserial'}, {'url' => 'https://pypi.python.org/packages/source/p/pypng/pypng-0.0.17.tar.gz', 'hash' => '2dfa74ac28a4c41ae61e62d243410548c7c174bd990528d30270324f15211544', 'name' => 'pypng'}, {'url' => 'https://pypi.python.org/packages/source/o/oauth2client/oauth2client-1.3.tar.gz', 'hash' => '668ffe5a9a6df1f5fca50a851da9ce97286dc10f190b05e84763a8cd9716141b', 'name' => 'oauth2client'}, {'url' => 'https://pypi.python.org/packages/source/b/backports.ssl_match_hostname/backports.ssl_match_hostname-3.4.0.2.tar.gz', 'hash' => '07410e7fb09aab7bdaf5e618de66c3dac84e2e3d628352814dc4c37de321d6ae', 'name' => 'backports.ssl-match-hostname'}, {'url' => 'https://pypi.python.org/packages/source/g/gevent/gevent-1.0.1.tar.gz', 'hash' => '4627e215d058f71d95e6b26d9e7be4c263788a4756bd2858a93775f6c072df43', 'name' => 'gevent'}, {'url' => 'https://pypi.python.org/packages/source/g/gevent-websocket/gevent-websocket-0.9.3.tar.gz', 'hash' => '6475220340f9f8895a0f51bd2b9df3511bc7765dc055f49e997584bdaee3381f', 'name' => 'gevent-websocket'}, {'url' => 'https://pypi.python.org/packages/source/g/greenlet/greenlet-0.4.5.zip', 'hash' => 'b990f00f120c4cfb6672eec2acd32210990a2c7b70b815c034d1d8cbf08e2dbe', 'name' => 'greenlet'}, {'url' => 'https://pypi.python.org/packages/source/p/peewee/peewee-2.4.7.tar.gz', 'hash' => '8ad1c4fb202332a969da83a0af712bca96ed6e2a70ca1523ab3d2a2234ed47bd', 'name' => 'peewee'}, {'url' => 'https://pypi.python.org/packages/source/p/pygeoip/pygeoip-0.3.2.tar.gz', 'hash' => 'f22c4e00ddf1213e0fae36dc60b46ee7c25a6339941ec1a975539014c1f9a96d', 'name' => 'pygeoip'}, {'url' => 'https://pypi.python.org/packages/source/p/python-dateutil/python-dateutil-2.4.1.post1.tar.gz', 'hash' => 'aa9bdbd60c395db90204609f1fb5aeb3797870f65c09f04f243476d22f8f4615', 'name' => 'python-dateutil'}, {'url' => 'https://pypi.python.org/packages/source/r/requests/requests-2.5.0.tar.gz', 'hash' => 'd2daef4919fc87262b8b3cb5a9d214cac8ce1e50950f8423bbc1d31c2e63d38e', 'name' => 'requests'}, {'url' => 'https://pypi.python.org/packages/source/s/six/six-1.8.0.tar.gz', 'hash' => '047bbbba41bac37c444c75ddfdf0573dd6e2f1fbd824e6247bb26fa7d8fa3830', 'name' => 'six'}, {'url' => 'https://pypi.python.org/packages/source/w/wsgiref/wsgiref-0.1.2.zip', 'hash' => 'c7e610c800957046c04c8014aab8cce8f0b9f0495c8cd349e57c1f7cabf40e79', 'name' => 'wsgiref'}]
+  PEBBLE_SDK_VERSION='3.0-dp9'
+  PEBBLE_SDK_SHA256='2338974ccf83256d29c61dcb9508cf35bac5e14fd8138f5c0cd5139010bcd411'
+
   homepage 'https://developer.getpebble.com'
-  url 'http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk2/PebbleSDK-3.0-dp8.tar.gz'
-  sha256 '6d3644bec91f0f9af0adbdbee5ce595d1772e47d2b6e52b45cbc45ea75048cce'
-  version '3.0-dp8'
+  url "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk2/PebbleSDK-#{PEBBLE_SDK_VERSION}.tar.gz"
+  sha256 "#{PEBBLE_SDK_SHA256}"
+  version "#{PEBBLE_SDK_VERSION}"
 
   depends_on 'freetype' => :recommended
 
@@ -13,98 +18,40 @@ class PebbleSdk < Formula
   depends_on 'glib'
   depends_on 'pixman'
 
-  # List of resources can be obtained from requirements.txt
-  resource 'freetype-py' do
-    url 'https://pypi.python.org/packages/source/f/freetype-py/freetype-py-1.0.tar.gz'
-    sha1 '3830e45ff9e9a96f1e209d786cbd5492f168127a'
+  PEBBLE_SDK_REQUIREMENTS.each do |r|
+    resource r["name"] do
+      url r["url"]
+      sha256 r["hash"]
+    end
   end
 
-  resource 'sh' do
-    url 'https://pypi.python.org/packages/source/s/sh/sh-1.09.tar.gz'
-    sha1 '995c3cd38ddc065088f5fa991206665ec899e938'
+  def cancel_install
+    Curses.close_screen
+    puts "To use the Pebble SDK, you must agree to the Pebble Terms of Use and Pebble Developer License."
+    puts "Cancelling installation of Pebble SDK..."
   end
 
-  resource 'twisted' do
-    url 'https://pypi.python.org/packages/source/T/Twisted/Twisted-12.0.0.tar.bz2'
-    sha1 '64b7f7fdeefbd4dd8e6bdffb12d9095106ee3d5d'
-  end
+  def check_license_agreement
+    Curses.noecho
+    Curses.init_screen    
+    
+    Curses.addstr("To use the Pebble SDK, you must agree to the following:\n\nPEBBLE TERMS OF USE\nhttps://developer.getpebble.com/legal/terms-of-use\n\nPEBBLE DEVELOPER LICENSE\nhttps://developer.getpebble.com/legal/sdk-license\n\nDo you accept the Pebble Terms of Use and the Pebble Developer License (y/n)? ")
 
-  resource 'autobahn' do
-    url 'https://pypi.python.org/packages/source/a/autobahn/autobahn-0.5.14.zip'
-    sha1 '475ba5f281bdcc50858c6920c034a1a067b2ce2a'
-  end
+    loop do
+      case Curses.getch
+        when 'y'
+          break
+        when 'n'
+          cancel_install
+          exit
+      end
+    end
 
-  resource 'websocket-client' do
-    url 'https://pypi.python.org/packages/source/w/websocket-client/websocket-client-0.22.0.tar.gz'
-    sha1 'f6d42683cf58b3a40b47ff0c78afca980df05abd'
-  end
-
-  resource 'pyserial' do
-    url 'https://pypi.python.org/packages/source/p/pyserial/pyserial-2.7.tar.gz'
-    sha1 'f15694b1bea9e4369c1931dc5cf09e37e5c562cf'
-  end
-
-  resource 'pypng' do
-    url 'https://pypi.python.org/packages/source/p/pypng/pypng-0.0.17.tar.gz'
-    sha1 '3821c58be15e833fc183dd361df02dece32ba180'
-  end
-
-  resource 'oauth2client' do
-    url 'https://pypi.python.org/packages/source/o/oauth2client/oauth2client-1.3.tar.gz'
-    sha1 '02c69758754be96c902225f3e55965cd5a392064'
-  end
-
-  resource 'backports.ssl_match_hostname' do
-    url 'https://pypi.python.org/packages/source/b/backports.ssl_match_hostname/backports.ssl_match_hostname-3.4.0.2.tar.gz'
-    sha1 'da4e41f3b110279d2382df47ac1e4f10c63cf954'
-  end
-
-  resource 'gevent' do
-    url 'https://pypi.python.org/packages/source/g/gevent/gevent-1.0.1.tar.gz'
-    sha1 '2cc1b6e1fa29b30ea881fa6a195e57faaf089ae8'
-  end
-
-  resource 'gevent-websocket' do
-    url 'https://pypi.python.org/packages/source/g/gevent-websocket/gevent-websocket-0.9.3.tar.gz'
-    sha1 '06da7a53cb3844a4094ce9279cfd6f70bde12172'
-  end
-
-  resource 'greenlet' do
-    url 'https://pypi.python.org/packages/source/g/greenlet/greenlet-0.4.5.zip'
-    sha1 '97f18d651595bd30243ad2f4702764791e57fa6e'
-  end
-
-  resource 'peewee' do
-    url 'https://pypi.python.org/packages/source/p/peewee/peewee-2.4.7.tar.gz'
-    sha1 '7803726847e109a3f56b456a092b04937ef3b6d2'
-  end
-
-  resource 'pygeoip' do
-    url 'https://pypi.python.org/packages/source/p/pygeoip/pygeoip-0.3.2.tar.gz'
-    sha1 '608365dc820b54ac44d368e6e33766444e0a5098'
-  end
-
-  resource 'python-dateutil' do
-    url 'https://pypi.python.org/packages/source/p/python-dateutil/python-dateutil-2.4.1.post1.tar.gz'
-    sha1 '55c712dac45ad14532d9bc05b64ba7e4bf6b56c6'
-  end
-
-  resource 'requests' do
-    url 'https://pypi.python.org/packages/source/r/requests/requests-2.5.0.tar.gz'
-    sha1 'd60dfaaa0b4b62a6646fcb6c3954ea369317ca9f'
-  end
-
-  resource 'six' do
-    url 'https://pypi.python.org/packages/source/s/six/six-1.8.0.tar.gz'
-    sha1 'aa3b0659cbc85c6c7a91efc51f2d1007040070cd'
-  end
-
-  resource 'wsgiref' do
-    url 'https://pypi.python.org/packages/source/w/wsgiref/wsgiref-0.1.2.zip'
-    sha1 '80b7e9b039e40a2f8419e00b393a6516d80cf8de'
+    Curses.close_screen
   end
 
   def install
+    check_license_agreement
     inreplace 'bin/pebble' do |s|
       # This replacement fixes a path that gets messed up because of the
       # bin.env_script_all_files call (which relocates actual pebble.py script
@@ -121,12 +68,10 @@ class PebbleSdk < Formula
     ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
     install_args = [ "setup.py", "install", "--prefix=#{libexec}" ]
 
-    %w[freetype-py sh twisted autobahn websocket-client pyserial pypng oauth2client
-       backports.ssl_match_hostname gevent gevent-websocket greenlet peewee pygeoip
-       python-dateutil requests six wsgiref].each do |r|
-      resource(r).stage { system "python", *install_args }
+    PEBBLE_SDK_REQUIREMENTS.each do |r|
+      resource(r["name"]).stage { system "python", *install_args }
     end
-
+    
     doc.install %w[Documentation Examples README.txt]
     prefix.install %w[Pebble bin tools requirements.txt version.txt]
 
@@ -146,8 +91,12 @@ class PebbleSdk < Formula
   end
 
   def caveats; <<-EOS.undent
-    Documentation and examples can be found in
+      
+      Documentation can be found online at https://developer.getpebble.com/docs or in
       #{doc}
+
+      Examples can be found online at https://github.com/pebble-examples
+
     EOS
   end
 end
