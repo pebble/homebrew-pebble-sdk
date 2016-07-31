452f2b7019c8ed34b4a0b9728e6e77346761541d
Matthew Berryman <mattjb@me.com>
2016-07-31 12:31:02 +1000

    Use sha256 in pebble-toolchain.rb

--- pebble-toolchain.rb
+++ pebble-toolchain.rb
@@ -3,7 +3,7 @@ require 'formula'
 class PebbleToolchain < Formula
   homepage 'https://github.com/pebble/arm-eabi-toolchain'
   url 'http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-macos-universal-static.tar.gz'
-  sha1 'b1baaf455140d3c6e3a889217bb83986fe6527a0'
+  sha256 '49a1ddaed13d44b928a7148fa344f9362d6ae6823496fb5077ea451230222c4b'
   version '2.0'
 
   keg_only 'Not linked to avoid conflicts.'
