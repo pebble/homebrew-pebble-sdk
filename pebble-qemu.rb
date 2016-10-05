640cbbf88903ab165a037162cc178aaed49213af
Katharine Berry <katharine@getpebble.com>
2016-10-05 14:38:58 -0700

    Add 2.5.0-pebble3 as devel

--- pebble-qemu.rb
+++ pebble-qemu.rb
@@ -1,15 +1,25 @@
 class PebbleQemu < Formula
   homepage "https://github.com/pebble/qemu"
-  url "https://github.com/pebble/qemu/archive/v2.5.0-pebble2.zip"
-  sha256 "c0fc5ad44c011a53a1c676cdd29e620f90838546f36f47b55cc8a2de9215b621"
-  head "https://github.com/pebble/qemu.git"
-  version "2.5.0-pebble2"
 
   bottle do
     root_url "http://pebble-sdk-homebrew.s3.amazonaws.com"
     cellar :any
     sha256 "91cc0a6234f9d9d0e6843e072756c78854cb2fae71330d054288c06f5bae4017" => :el_capitan
   end
+
+  stable do
+    url "https://github.com/pebble/qemu/archive/v2.5.0-pebble2.zip"
+    sha256 "c0fc5ad44c011a53a1c676cdd29e620f90838546f36f47b55cc8a2de9215b621"
+    version "2.5.0-pebble2"
+  end
+
+  devel do
+    url "https://github.com/pebble/qemu/archive/v2.5.0-pebble3.zip"
+    sha256 "aeae67a9339d381761ba05b16358e438ad9d57779a3459acaa4335067d30d28d"
+    version "2.5.0-pebble3"
+  end
+
+  head "https://github.com/pebble/qemu.git"
 
   depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
@@ -26,7 +36,7 @@ class PebbleQemu < Formula
     system "./configure", "--disable-werror",
                           "--enable-debug",
                           '--target-list=arm-softmmu',
-                          "--extra-cflags=-DSTM32_UART_NO_BAUD_DELAY",
+                          "--extra-cflags=-DSTM32_UART_NO_BAUD_DELAY -DSKIP_QUIT_PROMPT",
                           "--prefix=#{prefix}",
                           "--disable-mouse",
                           "--disable-docs"
