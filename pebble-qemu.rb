08cdb1d7f2ae0fe0e020d67e643a92ecf5df768a
Katharine Berry <katharine@getpebble.com>
2016-10-05 15:58:20 -0700

    try no_weak_imports

--- pebble-qemu.rb
+++ pebble-qemu.rb
@@ -33,6 +33,7 @@ class PebbleQemu < Formula
   depends_on "libjpeg"
 
   def install
+    ENV.no_weak_imports
     system "./configure", "--disable-werror",
                           "--enable-debug",
                           '--target-list=arm-softmmu',
